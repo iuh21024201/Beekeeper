589327427ef886f5204957beb23c50035d9efb4a
huynhthanhhoangiuh2003 <huynhthanhhoangbinhdinh@gmail.com>
2024-12-04 16:41:34 +0700

    Add files via upload
    model quan-li-cua-hang

--- model/mQuanLyCuaHang.php
+++ model/mQuanLyCuaHang.php
@@ -1,26 +1,179 @@
 <?php
-    require_once __DIR__ . '/../config.php';
+include_once("ketnoi.php");
 
-//insert, update , delete -> su dung function
-function execute($sql) {
-    $conn = mysqli_connect(HOST,USERNAME,PASSWORD,DATABASE);
+class mQuanLyCuaHang
+{
+    public function getEmployeesByStore($storeID)
+    {
+        // Tạo kết nối CSDL
+        $conn = new mysqli("localhost", "root", "", "beekeeper");
+        if ($conn->connect_error) {
+            die("Kết nối thất bại: " . $conn->connect_error);
+        }
 
-    //querry
-    mysqli_query($conn, $sql);
+        // Truy vấn lấy nhân viên theo cửa hàng
+        $sql = "SELECT 
+                    nhanvien.ID_NhanVien,
+                    nhanvien.HoTen,
+                    nhanvien.username,
+                    nhanvien.SoDienThoai,
+                    nhanvien.Email,
+                    nhanvien.TrangThai,
+                    cuahang.TenCuaHang,
+                    cuahang.DiaChi,
+                    taikhoan.TenTaiKhoan,
+                    taikhoan.MatKhau,
+                    taikhoan.PhanQuyen
+                FROM nhanvien
+                JOIN cuahang ON nhanvien.ID_CuaHang = cuahang.ID_CuaHang
+                JOIN taikhoan ON nhanvien.ID_TaiKhoan = taikhoan.ID_TaiKhoan 
+                WHERE nhanvien.ID_CuaHang = ?";
 
-    //dong connect
-    mysqli_close($conn);
-}
-// su dung cho lech select va tra ve ket qua
-function executeResult($sql) {
-    $conn = mysqli_connect(HOST,USERNAME,PASSWORD,DATABASE);
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('i', $storeID);  // Bind ID cửa hàng vào câu truy vấn
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-    $resultset = mysqli_query($conn , $sql);
-    $list = [];
-    while ($row = mysqli_fetch_array($resultset, 1)) { 
-        $list[] = $row ;
+        $nhanvienlist = [];
+        while ($row = $result->fetch_assoc()) {
+            $nhanvienlist[] = $row;
+        }
+
+        $stmt->close();
+        $conn->close();
+
+        return $nhanvienlist;
     }
 
-    mysqli_close($conn);
-    return $list;
+    public function selectAllCuaHang()
+    {
+        $p = new clsketnoi();
+        $con = $p->moKetNoi();
+        $truyvan = "select * from cuahang";
+        $tbl = mysqli_query($con, $truyvan);
+        $p->dongKetNoi($con);
+        return $tbl;
+    }
+
+    public function selectAllNhanVien()
+    {
+        $p = new clsketnoi();
+        $con = $p->moKetNoi();
+        $truyvan = "SELECT 
+                    nhanvien.ID_NhanVien,
+                    nhanvien.HoTen,
+                    nhanvien.username,
+                    nhanvien.SoDienThoai,
+                    nhanvien.Email,
+                    nhanvien.TrangThai,
+                    cuahang.TenCuaHang,
+                    cuahang.DiaChi,
+                    taikhoan.TenTaiKhoan,
+                    taikhoan.MatKhau,
+                    taikhoan.PhanQuyen
+                FROM nhanvien
+                JOIN cuahang ON nhanvien.ID_CuaHang = cuahang.ID_CuaHang
+                JOIN taikhoan ON nhanvien.ID_TaiKhoan = taikhoan.ID_TaiKhoan";
+        $tbl = mysqli_query($con, $truyvan);
+        $p->dongKetNoi($con);
+        return $tbl;
+    }
+
+    public function getChiNhanhByID($id)
+    {
+        $p = new clsketnoi();
+        $con = $p->moKetNoi();
+
+        $truyvan = "SELECT 
+                    taikhoan.ID_TaiKhoan,
+                    cuahang.ID_CuaHang,
+                    cuahang.TenCuaHang,
+                    cuahang.DiaChi
+                FROM
+                    taikhoan
+                JOIN nhanvien ON taikhoan.ID_TaiKhoan = nhanvien.ID_TaiKhoan
+                JOIN cuahang ON nhanvien.ID_CuaHang = cuahang.ID_CuaHang
+                WHERE taikhoan.ID_TaiKhoan = ?";
+
+        $stmt = mysqli_prepare($con, $truyvan);
+        if ($stmt === false) {
+            echo "Lỗi chuẩn bị truy vấn: " . mysqli_error($con);
+            return null;
+        }
+
+        mysqli_stmt_bind_param($stmt, "i", $id);
+
+        mysqli_stmt_execute($stmt);
+
+        $result = mysqli_stmt_get_result($stmt);
+        $data = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row;
+        }
+
+        mysqli_stmt_close($stmt);
+        $p->dongKetNoi($con);
+
+        return $data;
+    }
+
+
+    public function selectOneNhanVien($id)
+    {
+        $p = new clsketnoi();
+        $con = $p->moKetNoi();
+
+        $truyvan = "SELECT 
+                    nhanvien.ID_NhanVien,
+                    nhanvien.HoTen,
+                    nhanvien.username,
+                    nhanvien.SoDienThoai,
+                    nhanvien.Email,
+                    nhanvien.TrangThai,
+                    cuahang.ID_CuaHang,
+                    cuahang.TenCuaHang,
+                    cuahang.DiaChi,
+                    taikhoan.TenTaiKhoan,
+                    taikhoan.MatKhau,
+                    taikhoan.PhanQuyen
+                FROM nhanvien
+                JOIN cuahang ON nhanvien.ID_CuaHang = cuahang.ID_CuaHang
+                JOIN taikhoan ON nhanvien.ID_TaiKhoan = taikhoan.ID_TaiKhoan WHERE nhanvien.ID_NhanVien = ?";
+
+        $stmt = mysqli_stmt_init($con);
+
+        if (mysqli_stmt_prepare($stmt, $truyvan)) {
+            mysqli_stmt_bind_param($stmt, 'i', $id);
+
+            mysqli_stmt_execute($stmt);
+
+            $result = mysqli_stmt_get_result($stmt);
+
+            if ($row = mysqli_fetch_assoc($result)) {
+                $tbl = $row;
+            } else {
+                $tbl = null;
+            }
+
+            mysqli_stmt_close($stmt);
+        } else {
+            $tbl = null;
+        }
+
+        $p->dongKetNoi($con);
+
+        return $tbl;
+    }
+
+
+
+    public function updateTrangThai($id, $trangthai)
+    {
+        $p = new clsketnoi();
+        $con = $p->moKetNoi();
+        $truyvan = "UPDATE nhanvien SET TrangThai = $trangthai WHERE ID_NhanVien = $id";
+        $tbl = mysqli_query($con, $truyvan);
+        $p->dongKetNoi($con);
+        return $tbl;
+    }
 }
